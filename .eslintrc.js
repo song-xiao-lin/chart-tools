de27b94927750e26cfbee333414fc5dbb8bc5ca8
songxiaolin <songxiaolin@chinamcloud.com>
2022-07-16 14:15:29 +0800

    eslint制定规范

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		},
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react"
+	],
+	"rules": {
+		"eol-last": 1, // 文件以单一的换行符结束
+		"no-multiple-empty-lines": [1, { "max": 2 }], // 空行不能超过两行
+		"no-multi-spaces": 1, // 不能有多余空格
+		"no-spaced-func": 1, // 函数调用时 函数名与()之间不能有空格
+		"no-trailing-spaces": 1, // 一行结束后面不要有空格
+		"no-use-before-define": 1, // 未定义前不能使用
+		"no-var": 1, // 禁用var，用let和const代替
+		"no-else-return": 2, // 如果if语句里面有return, 后面不能跟else语句
+		"no-empty": 1, // 块语句中的内容不能为空
+		"no-invalid-regexp": 2, // 禁止无效的正则表达式
+		"no-func-assign": 2, // 禁止重复的函数声明
+	}
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import { lazy } from "react";
-import { Navigate } from "react-router-dom";
+import React, { lazy } from 'react';
+import { Navigate } from 'react-router-dom';
 
-const Home = lazy(() => import("../pages/Home"));
+const Home = lazy(() => import('../pages/Home'));
 // const News = lazy(() => import("../pages/News"));
 
 const routes = [
-  { path: "/", element: <Navigate to={"/home"} /> },
+  { path: '/', element: <Navigate to={'/home'} /> },
   {
-    path: "/home",
+    path: '/home',
     element: <Home />,
     children: [
-      // { path: "", element: <News /> }
+      // { path: '', element: <News /> }
     ],
   },
 ]
